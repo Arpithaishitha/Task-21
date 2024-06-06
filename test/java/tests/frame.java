ef15f1a7e083e7664e781dfbe31b5eb1e0c7026a
Arpithaishitha <arpithass139@gmail.com>
2024-06-06 21:42:04 +0530

    adding task21 file into branch

---
+++ test/java/tests/frame.java
@@ -0,0 +1,37 @@
+package tests;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class frame extends mainChorme {
+
+    @Test
+    public void test1() {
+
+        //load url
+        chromeDriver.get("https://the-internet.herokuapp.com/iframe");
+
+        //locate frame xpath
+        WebDriverWait wait = new WebDriverWait(chromeDriver, Duration.ofSeconds(5));
+        wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(By.id("mce_0_ifr")));
+
+        //locate p tag inside frame
+        WebElement p = chromeDriver.findElement(By.id("tinymce"));
+
+        //Send hello people inside p tag using javascript
+        JavascriptExecutor js = (JavascriptExecutor) chromeDriver;
+        js.executeScript("arguments[0].innerText = 'Hello People';", p);
+
+        //verify the enter name is correct or not
+        String name = p.getText();
+        System.out.println(name);
+
+    }
+
+}
